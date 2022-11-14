43fec8c87a8611f34273a7e8f3c36bd4cf047bf2
s-miyazaki <seiichiro.miyazaki@dragon.jp>
2022-11-15 00:33:05 +0900

    entity作成

---
+++ api/src/items/entities/item.entity.ts
@@ -0,0 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Item } from '@prisma/client';
+
+export class ItemEntity implements Item {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  price: number;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+}
