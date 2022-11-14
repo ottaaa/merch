2ae59f3562543261051e634b18301392c6434090
s-miyazaki <seiichiro.miyazaki@dragon.jp>
2022-11-14 22:40:52 +0900

    main.tsでswaggerのセットアップ

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as pack from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('app');
+
+  const config = new DocumentBuilder()
+    .setTitle('物販(仮) Document')
+    .setDescription('物販(仮) の API リファレンスです')
+    .setVersion(pack.version)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
