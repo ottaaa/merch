5e93a4d392303b6f694c277369f9427fcbbff6ac
太田勝博 <styxcab@gmail.com>
2022-11-17 22:36:20 +0900

    generateした内容を追加

---
+++ front/gen/models/index.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CreateItemDto';
+export * from './ItemEntity';
+export * from './ResponseItemDto';
+export * from './ResponseItemDtoItem';
+export * from './ResponseItemsDto';
