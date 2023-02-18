72c4fcb7be824d092b9b200a393198044ba9e44d
Seiichiro Miyazaki <seiichiro.miyazaki@dragon.jp>
2023-02-18 22:29:55 +0900

    seeder追加

---
+++ api/db/prisma/seed.ts
@@ -0,0 +1,44 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      code: 'zackey',
+      email: 'zackey@email.com',
+      name: 'ザッキー',
+    },
+  });
+
+  const shop = await prisma.shop.create({
+    data: {
+      name: 'zackey merch',
+    },
+  });
+
+  await prisma.seller.create({
+    data: {
+      userId: user.id,
+      shopId: shop.id,
+      role: 'OWNER',
+    },
+  });
+
+  await prisma.merch.create({
+    data: {
+      name: 'T-Shirt',
+      description: '2023年Tシャツ',
+      image: 'image',
+      shopId: shop.id,
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
