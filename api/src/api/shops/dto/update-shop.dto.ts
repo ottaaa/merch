6df103db64f734255c06859a475b46abd44d5d49
Seiichiro Miyazaki <seiichiro.miyazaki@dragon.jp>
2023-04-22 01:13:01 +0900

    dtoのバリデーションを修正

--- api/src/api/shops/dto/update-shop.dto.ts
+++ api/src/api/shops/dto/update-shop.dto.ts
@@ -1,10 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-import { Min, Max } from 'class-validator';
+import { IsString, Length } from 'class-validator';
 
 export class UpdateShopDto {
-  @Min(1)
-  @Max(128)
+  @IsString()
+  @Length(1, 128)
   @ApiProperty({ required: false, minLength: 1, maxLength: 128, example: '放課後ティータイム' })
   name?: string;
 }
