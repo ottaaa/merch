97f850de70269c77fd4b550de2037b82518c8d51
Seiichiro Miyazaki <seiichiro.miyazaki@dragon.jp>
2023-04-22 01:12:38 +0900

    shopのidをレスポンスから除外していたのを修正

--- api/src/models/shop.model.ts
+++ api/src/models/shop.model.ts
@@ -2,12 +2,9 @@ import { Shop } from '@prisma/client';
 
 import { ApiProperty } from '@nestjs/swagger';
 
-import { Exclude } from 'class-transformer';
-
 import { PrismaObjectToEntity, Fields } from './types/model.type';
 
 export class ShopModel implements PrismaObjectToEntity<Shop> {
-  @Exclude({ toPlainOnly: true })
   @ApiProperty({ example: 1 })
   readonly id?: number;
 
