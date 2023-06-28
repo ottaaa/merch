a1b55f1b3180c683c767950fde7257c1f3d45d2b
Seiichiro Miyazaki <seiichiro.miyazaki@dragon.jp>
2023-06-28 23:59:20 +0900

    testing moduleのDIを修正

--- api/src/api/merchs/merchs.controller.spec.ts
+++ api/src/api/merchs/merchs.controller.spec.ts
@@ -1,11 +1,15 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { AuthorizationModule } from '#/src/authorization/authorization.module';
+
 import { MerchsController } from './merchs.controller';
+
+import { Test, TestingModule } from '@nestjs/testing';
 
 describe('MerchsController', () => {
   let controller: MerchsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [AuthorizationModule],
       controllers: [MerchsController],
     }).compile();
 
